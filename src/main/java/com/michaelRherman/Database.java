028fe84a4a88b28f7934f84ea0c0462d9bc8341f
michaelrherman <michaelrherman@gmail.com>
2015-04-25 12:14:06 -0500

    Added method to open website in default web browser.

---
+++ src/main/java/com/michaelRherman/Database.java
@@ -0,0 +1,4 @@
+package com.michaelRherman;
+
+public class Database {
+}
--- src/main/java/com/michaelRherman/Main.java
+++ src/main/java/com/michaelRherman/Main.java
@@ -1,8 +1,25 @@
 package com.michaelRherman;
 
+import java.awt.Desktop;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
         new MashupGUI();
+    }
+
+    public static void openDefaultBrowser() throws Exception {
+        try {
+            if (Desktop.isDesktopSupported()) {
+                Desktop.getDesktop().browse(new URI("http://www.google.com"));
+            }
+        } catch (URISyntaxException USE) {
+            USE.printStackTrace();
+        } catch (Exception E) {
+            E.printStackTrace();
+        }
     }
 }
